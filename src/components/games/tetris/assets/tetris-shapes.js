f2051907f39fb838cadd542dd17eee46efa38033
plrtp <plrtp@mail.ru>
2025-01-23 12:26:36 +0300

    добавлены компоненты, директории, формирующие структуру тетриса

---
+++ src/components/games/tetris/assets/tetris-shapes.js
@@ -0,0 +1,37 @@
+const shapes = {
+    'I': {
+        1: [{color: 'blue', x: 0, y: 0}, {color: 'blue', x: 0, y: 20}, {color: 'blue', x: 0, y: 40}, {color: 'blue', x: 0, y: 60}],
+        2: [{color: 'blue', x: 0, y: 0}, {color: 'blue', x: 20, y: 0}, {color: 'blue', x: 40, y: 0}, {color: 'blue', x: 60, y: 0}],
+    },
+    'J': {
+        1: [{color: 'pink', x: 0, y: 40}, {color: 'pink', x: 20, y: 0}, {color: 'pink', x: 20, y: 20}, {color: 'pink', x: 20, y: 40}],
+        2: [{color: 'pink', x: 0, y: 0}, {color: 'pink', x: 0, y: 20}, {color: 'pink', x: 20, y: 20}, {color: 'pink', x: 40, y: 20}],
+        3: [{color: 'pink', x: 0, y: 0}, {color: 'pink', x: 0, y: 20}, {color: 'pink', x: 0, y: 40}, {color: 'pink', x: 20, y: 0}],
+        4: [{color: 'pink', x: 0, y: 20}, {color: 'pink', x: 20, y: 20}, {color: 'pink', x: 40, y: 0}, {color: 'pink', x: 40, y: 20}],
+    },
+    'L': {
+        1: [{color: 'orange', x: 0, y: 0}, {color: 'orange', x: 0, y: 20}, {color: 'orange', x: 0, y: 40}, {color: 'orange', x: 20, y: 40}],
+        2: [{color: 'orange', x: 0, y: 0}, {color: 'orange', x: 0, y: 20}, {color: 'orange', x: 20, y: 0}, {color: 'orange', x: 40, y: 0}],
+        3: [{color: 'orange', x: 0, y: 0}, {color: 'orange', x: 20, y: 0}, {color: 'orange', x: 20, y: 20}, {color: 'orange', x: 20, y: 40}],
+        4: [{color: 'orange', x: 0, y: 20}, {color: 'orange', x: 20, y: 20}, {color: 'orange', x: 40, y: 0}, {color: 'orange', x: 40, y: 20}],
+    },
+    'O': {
+        1: [{color: 'yellow', x: 0, y: 0}, {color: 'yellow', x: 0, y: 20}, {color: 'yellow', x: 20, y: 0}, {color: 'yellow', x: 20, y: 20}],
+    },
+    'S': {
+        1: [{color: 'red', x: 0, y: 20}, {color: 'red', x: 20, y: 0}, {color: 'red', x: 20, y: 20}, {color: 'red', x: 40, y: 0}],
+        2: [ {color: 'red', x: 0, y: 0}, {color: 'red', x: 0, y: 20}, {color: 'red', x: 20, y: 20}, {color: 'red', x: 20, y: 40}],
+    },
+    'Z': {
+        1: [{color: 'green', x: 0, y: 0}, {color: 'green', x: 20, y: 0}, {color: 'green', x: 20, y: 20}, {color: 'green', x: 40, y: 20}],
+        2: [{color: 'green', x: 0, y: 20}, {color: 'green', x: 0, y: 40}, {color: 'green', x: 20, y: 0}, {color: 'green', x: 20, y: 20}],
+    },
+    'T': {
+        1: [{color: 'purple', x: 0, y: 0}, {color: 'purple', x: 20, y: 0}, {color: 'purple', x: 20, y: 20}, {color: 'purple', x: 40, y: 0}],
+        2: [{color: 'purple', x: 0, y: 20}, {color: 'purple', x: 20, y: 0}, {color: 'purple', x: 20, y: 20}, {color: 'purple', x: 20, y: 40}],
+        3: [{color: 'purple', x: 0, y: 20}, {color: 'purple', x: 20, y: 0}, {color: 'purple', x: 20, y: 20}, {color: 'purple', x: 40, y: 20}],
+        4: [{color: 'purple', x: 0, y: 0}, {color: 'purple', x: 0, y: 20}, {color: 'purple', x: 0, y: 40}, {color: 'purple', x: 20, y: 20}],
+    }
+};
+
+export default shapes;
