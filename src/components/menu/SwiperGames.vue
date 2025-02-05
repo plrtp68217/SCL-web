a86b4d3a319ef11f656aed8e400f6d48ed62d78f
plrtp <plrtp@mail.ru>
2025-02-05 20:51:26 +0300

    рефакторинг

--- src/components/menu/SwiperGames.vue
+++ src/components/menu/SwiperGames.vue
@@ -36,16 +36,16 @@ const props = defineProps<{
 const modules = ref([EffectCoverflow, Navigation, Autoplay]);
 
 const coverflowEffect =  ref({
-  rotate: 30, // Угол поворота слайдов
+  rotate: 50, // Угол поворота слайдов
   stretch: 0, // Растяжение слайдов
   depth: 200, // Глубина эффекта
   modifier: 1, // Модификатор эффекта
-  slideShadows: false, // Включаем тени
+  slideShadows: false, // Выключаем тени
 });
 
 const autoplayOptions = {
   delay: 3000, // Задержка между слайдами (3 секунды)
-  disableOnInteraction: true, // Не останавливать автопрокрутку при взаимодействии
+  disableOnInteraction: true, // Останавливать автопрокрутку при взаимодействии
 }
 
 </script>
