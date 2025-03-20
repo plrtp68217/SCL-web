3790f8e852e7a7ce4f201dc110c5fd68f232e96d
plrtp <plrtp@mail.ru>
2025-03-20 12:51:13 +0300

    add: wolf->egg + draw, move

--- src/components/games/wolf/classes/Egg.ts
+++ src/components/games/wolf/classes/Egg.ts
@@ -2,13 +2,24 @@ export class Egg {
     x: number;
     y: number;
 
-    width: number;
-    height: number;
+    radius: number;
 
-    constructor(x: number, y: number, width: number, height: number) {
+    constructor(x: number, y: number, radius: number) {
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.radius = radius;
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        context.fill();
+    }
+
+    move(angle: number, step: number, directon: -1 | 1) {
+        const deltaX = directon * step * Math.cos(angle);
+        const deltaY = directon * step * Math.sin(angle);
+
+        this.x += deltaX;
+        this.y += deltaY;
     }
 }
