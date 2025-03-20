8b3b112c3ad000f89c35a473c95566b5f3843c9d
plrtp <plrtp@mail.ru>
2025-03-20 19:51:04 +0300

    add: Modal.vue - анимация появления

--- src/components/UI/Modal.vue
+++ src/components/UI/Modal.vue
@@ -12,12 +12,14 @@
 
 
 <style scoped>
+@import url(../../animations/appear.css);
 
 .modal {
   position: absolute;
   inset: 0;
   backdrop-filter: blur(5px);
   border: 2px solid rgba(108, 8, 165, 0.603);
+  animation: appear 600ms ease-in-out;
 }
 
 </style>
