72744663f5271783769f870936fdd55bd4d3af85
plrtp <plrtp@mail.ru>
2025-06-13 18:29:36 +0300

    update: сделан api records

---
+++ src/api/records.api.ts
@@ -0,0 +1,24 @@
+import apiClient from "./apiClient";
+
+import type { Record, CreateRecordDto,  UpdateRecordDto } from "./types/records";
+
+
+export default {
+  async getRecord(userId: number, gameId: string): Promise<Record | null> {
+    const response = await apiClient.get(`/records/${userId}/${gameId}`);
+    const record = response.data;
+    return record;
+  },
+
+  async createRecord(dto: CreateRecordDto): Promise<Record> {
+    const response = await apiClient.post('/records', dto);
+    const record = response.data;
+    return record;
+  },
+
+  async updateRecord(dto: UpdateRecordDto): Promise<Record> {
+    const response = await apiClient.put('/records', dto);
+    const record = response.data;
+    return record;
+  }
+}
--- src/api/types/records.ts
+++ src/api/types/records.ts
@@ -3,3 +3,14 @@ export interface Record {
   gameId: string,
   score: number,
 }
+
+export interface CreateRecordDto {
+  userId: number,
+  gameId: string,
+}
+
+export interface UpdateRecordDto {
+  userId: number,
+  gameId: string,
+  score: number,
+}
