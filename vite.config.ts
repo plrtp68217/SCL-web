e126a5ee6fa8a6af9250af1382e22daadf698eac
plrtp <plrtp@mail.ru>
2025-06-20 22:00:21 +0300

    update: vite.config - удален allowedHost

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     }
   },
-  server: {
-    allowedHosts: [
-      'fe52f788f938a04e5e99a0c32ecef1d0.serveo.net', // сюда записать адрес из https://serveo.net/
-      'localhost',
-    ],
-  },
 });
