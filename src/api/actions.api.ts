9af918c0a67ac773d502462da6f097d4e92aab3a
plrtp <plrtp@mail.ru>
2025-06-24 12:42:08 +0300

    update: типизировано api actions

--- src/api/actions.api.ts
+++ src/api/actions.api.ts
@@ -1,32 +1,32 @@
 import apiClient from "./apiClient";
-import type { CreateActionDto } from "./types/actions";
+import type { Action, CreateActionDto, IGameActivity, IGameActivityInterval, ISummary, ISummaryInterval } from "./types/actions";
 
 export default {
-  async getSummary() {
+  async getSummary(): Promise<ISummary> {
     const response = await apiClient.get('/actions/summary');
     const summary = response.data;
     return summary;
   },
 
-  async getSummaryInterval(date_start: Date, date_end: Date) {
+  async getSummaryInterval(date_start: Date, date_end: Date): Promise<ISummaryInterval> {
     const response = await apiClient.get(`/actions/summary/${date_start}/${date_end}`);
     const summaryInterval = response.data;
     return summaryInterval;
   },
 
-  async getGameActivity(gameId: string) {
+  async getGameActivity(gameId: string): Promise<IGameActivity> {
     const response = await apiClient.get(`/actions/game/${gameId}`);
     const gameActivity = response.data;
     return gameActivity;
   },
 
-  async getGameActivityInterval(gameId: string, date_start: Date, date_end: Date) {
+  async getGameActivityInterval(gameId: string, date_start: Date, date_end: Date): Promise<IGameActivityInterval> {
     const response = await apiClient.get(`/actions/game/${gameId}/${date_start}/${date_end}`);
     const gameActivityInterval = response.data;
     return gameActivityInterval;
   },
 
-  async createAction(dto: CreateActionDto) {
+  async createAction(dto: CreateActionDto): Promise<Action> {
     const response = await apiClient.post('/actions', dto);
     const action = response.data;
     return action;
--- src/api/types/actions.ts
+++ src/api/types/actions.ts
@@ -1,4 +1,28 @@
+export interface ISummary {
+  entrysCount: number | string;
+  actionsCount: number | string;
+}
+
+export interface ISummaryInterval {
+  entrysCountInterval: number | string;
+  actionsCountInterval: number | string;
+}
+
+export interface IGameActivity {
+  gameActivity: number | string;
+}
+
+export interface IGameActivityInterval {
+  gameActivityInterval: number | string;
+}
+
 export interface CreateActionDto {
   name: string,
   action: string,
+}
+
+export interface Action {
+  name: string;
+  action: string;
+  date: Date;
 }
