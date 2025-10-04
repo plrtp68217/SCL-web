85091aeb8dc42c59268e1f62d9beaec0462d3f41
plrtp <plrtp@mail.ru>
2025-10-04 15:28:38 +0300

    update: вывод объекта телеграм про порлучении данных пользователя

--- src/telegram/useTelegram.ts
+++ src/telegram/useTelegram.ts
@@ -6,6 +6,8 @@ interface TelegramData {
 export function getTelegramData(): TelegramData { // переименовать либо файл либо функцию
   const telegram = window.Telegram.WebApp;
 
+  console.log(telegram);
+  
   return {
     userId: telegram.initDataUnsafe.user?.id,
     name: telegram.initDataUnsafe.user?.first_name,
