19527d7218fec5e4e7c21a8ff7734781bdecab22
plrtp <plrtp@mail.ru>
2025-10-20 20:13:08 +0300

    update: добавил usersApi в индекс

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import authorizationApi from "./authorization.api";
+import usersApi from "./users.api";
 import recordsApi from "./records.api";
 import actionsApi from "./actions.api";
 import channelsApi from './channels.api'
 
 export const api = {
   auth: authorizationApi,
+  users: usersApi,
   records: recordsApi,
   actions: actionsApi,
   channels: channelsApi,
